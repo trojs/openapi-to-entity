48e6a62812e374d7224abc1121a4bfb2c7d463c6
Pieter Wigboldus <w3n@w3news.org>
2024-07-25 19:48:18 +0200

    Update dependencies and finetune types

--- src/index.js
+++ src/index.js
@@ -1,33 +1,37 @@
 /**
+ * @typedef {import('openapi-types').OpenAPIV3.SchemaObject} SchemaObject
+ * @typedef {import('openapi-types').OpenAPIV3.NonArraySchemaObjectType} NonArraySchemaObjectType
+ */
+/**
+ * Find Type
  * @param {object} params
- * @param {string} params.type
+ * @param {NonArraySchemaObjectType} params.type
  * @returns {string}
  */
 const findType = ({ type }) => {
-  if (type === 'number') {
-    return 'int'
-  }
+    if (type === 'number') {
+        return 'int';
+    }
 
-  if (type === 'object') {
-    return 'json'
-  }
+    if (type === 'object') {
+        return 'json';
+    }
 
-  return 'varchar'
-}
+    return 'varchar';
+};
 
 /**
+ * OpenAPI to Entity
  * @param {object} params
- * @param {object} params.openapiSchema
+ * @param {SchemaObject} params.openapiSchema
  * @returns {object}
  */
-export default ({ openapiSchema }) => Object.fromEntries(
-  Object.entries(openapiSchema.properties)
-    .map(
-      ([key, value]) => {
-        const newValue = {
-          type: findType({ type: value.type })
-        }
-        return [key, newValue]
-      }
-    )
-)
+export default ({ openapiSchema }) =>
+    Object.fromEntries(
+        Object.entries(openapiSchema.properties).map(([key, value]) => {
+            const newValue = {
+                type: findType({ type: value.type }),
+            };
+            return [key, newValue];
+        })
+    );
